15b2a6030bbcbe8db48ce0068a182d8c2fed8684
Miki Palao <miki.palao@mrmilu.com>
2025-10-20 18:09:33 +0200

    remove instructions from abstract factory implementation

--- creational/abstract-factory/abstract-factory.ts
+++ creational/abstract-factory/abstract-factory.ts
@@ -1,20 +1,3 @@
-/**
- * !Instructions:
-	1. Complete the Product Classes:
-	•	ElectricCar should implement Vehicle and display the message "Assembling an electric car".
-	•	GasCar should implement Vehicle and display the message "Assembling a gas car".
-	•	ElectricEngine should implement Engine and display the message "Starting electric engine".
-	•	GasEngine should implement Engine and display the message "Starting gas engine".
-
-	2. Complete the Factory Classes:
-	•	ElectricVehicleFactory should create an ElectricCar and an ElectricEngine.
-	•	GasVehicleFactory should create a GasCar and a GasEngine.
-
-	3. Test the Code:
-	•	Run the code to ensure that each factory produces the correct type of vehicle and engine.
-
- */
-
 interface Vehicle {
   assemble(): void;
 }
